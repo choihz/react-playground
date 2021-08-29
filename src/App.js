a5798adf00e494facfe8d954d5c2d9bbcc4f5b18
choihz <choixhz@gmail.com>
2021-08-30 01:22:27 +0900

    PropTypes 추가 및 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return <div>시작</div>;
+const App = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
 export default App;
