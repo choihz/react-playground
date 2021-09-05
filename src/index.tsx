dc18697afda77adff1cbaef042175130d1330494
choihz <choixhz@gmail.com>
2021-09-05 20:47:30 +0900

    타입스크립트 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+
+const App = () => {
+  return <div>안녕</div>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
