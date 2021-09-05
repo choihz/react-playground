5e321bd139480f90f16555e83a5196a95176cd8f
choihz <choixhz@gmail.com>
2021-09-05 16:35:58 +0900

    테스트 대상 DOM 엘리먼트를 testid로 찾기

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -7,7 +7,12 @@ const Checkbox = () => {
     <>
       <label>
         {checked ? 'checked' : 'not checked'}
-        <input type="checkbox" value={checked} onChange={setChecked} />
+        <input
+          type="checkbox"
+          value={checked}
+          onChange={setChecked}
+          data-testid="checkbox"
+        />
       </label>
     </>
   );
--- src/components/Checkbox.test.js
+++ src/components/Checkbox.test.js
@@ -3,8 +3,8 @@ import { render, fireEvent } from '@testing-library/react';
 import Checkbox from './Checkbox';
 
 test('Selecting the checkbox should toggle its value', () => {
-  const { getByLabelText } = render(<Checkbox />);
-  const checkbox = getByLabelText(/not checked/i);
+  const { getByTestId } = render(<Checkbox />);
+  const checkbox = getByTestId('checkbox');
 
   fireEvent.click(checkbox);
   expect(checkbox.checked).toEqual(true);
