8e7566b9f092a026deee928f5d64da22c19d9540
choihz <choixhz@gmail.com>
2021-09-05 16:26:16 +0900

    이벤트 테스트

---
+++ src/components/Checkbox.js
@@ -0,0 +1,16 @@
+import React, { useReducer } from 'react';
+
+const Checkbox = () => {
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+
+  return (
+    <>
+      <label>
+        {checked ? 'checked' : 'not checked'}
+        <input type="checkbox" value={checked} onChange={setChecked} />
+      </label>
+    </>
+  );
+};
+
+export default Checkbox;
